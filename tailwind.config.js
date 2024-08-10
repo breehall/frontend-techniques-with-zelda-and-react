100cdb6e1add16317f8037e5913884e0a3919349
Bree Hall <40739624+breehall@users.noreply.github.com>
2024-08-10 16:59:01 -0400

    Project config and initialization

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      'hyrule-green': '#14532d',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
