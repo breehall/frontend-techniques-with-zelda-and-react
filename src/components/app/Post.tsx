b815fe3988bdf0a6a7782e985c8374e7a02c6783
Bree Hall <briannajdhall@gmail.com>
2024-08-12 00:31:55 -0400

    [useMemo] Created the Feed component and made a few tweaks to the Post component; added accompanying docs to slides

--- src/components/app/Post.tsx
+++ src/components/app/Post.tsx
@@ -3,11 +3,13 @@ export type PostProps = {
   username: string;
   name: string;
   avatar: string;
-  age: string;
+  age?: string;
+  datePosted?: string;
   content: string;
   likes: number;
   comments: number;
   saves: number;
+  onLike?: (postId: number) => void;
 };
 
 const Post = ({
@@ -16,20 +18,23 @@ const Post = ({
   name,
   avatar,
   age,
+  datePosted,
   content,
   likes,
   comments,
   saves,
+  onLike,
 }: PostProps) => {
   return (
     <article
       id={`post-${postId}`}
-      className="flex flex-col p-4 pb-4 mb-3 border-2 border-stone-100 rounded-xl gap-4"
+      className="flex flex-col p-4 pb-4 mb-3 border-2 text-sm border-stone-100 rounded-xl gap-4 bg-white shadow-sm"
     >
       <PostHeader
         name={name}
         username={username}
         age={age}
+        datePosted={datePosted}
         avatar={avatar}
         content={content}
       />
@@ -40,6 +45,7 @@ const Post = ({
           count={likes}
           label={`Like post with ${likes} likes`}
           postId={postId}
+          onClick={() => onLike && onLike(postId)}
         />
         <PostInteractionButton
           icon="comments"
@@ -64,19 +70,21 @@ const PostHeader = ({
   username,
   avatar,
   age,
+  datePosted,
   content,
 }: Partial<PostProps>) => {
   return (
-    <div className="flex flex-row gap-x-4">
+    <div className="flex flex-row gap-x-4 ">
       <img
         src={avatar}
         alt={`${name}'s profile picture`}
-        className="w-12 h-12 rounded-full"
+        className="w-12 h-12 rounded-full object-cover"
       />
       <div>
         <p className="font-bold">
           {name} <span className="font-normal">@{username}</span> ·{" "}
-          <span className="font-normal">{age}</span>
+          {age && <span className="font-normal">{age}</span>}
+          {datePosted && <span className="font-normal">{datePosted}</span>}
         </p>
         <p>{content}</p>
       </div>
@@ -89,6 +97,7 @@ type PostInteractionButtonProps = {
   count: number;
   label: string;
   postId: number;
+  onClick?: () => void;
 };
 
 const PostInteractionButton = ({
@@ -96,12 +105,14 @@ const PostInteractionButton = ({
   count,
   label,
   postId,
+  onClick,
 }: PostInteractionButtonProps) => {
   return (
     <button
       className="text-stone-400 flex items-center justify-center gap-2"
       aria-label={label}
       key={`post-interaction-${icon}-${postId}`}
+      onClick={onClick}
     >
       <i className={`lni lni-${icon}`}></i> {count}
     </button>
